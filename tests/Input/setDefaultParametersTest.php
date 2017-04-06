891106cdd1ce1724037b3ab5218ca0a6b340e6ca
Liviu Gelea <liviu.gelea@gmail.com>
2017-04-07 00:24:07 +0300

    Added phpunit framework and a sample test

---
+++ tests/Input/setDefaultParametersTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Leaveyou\Console\Tests\Input;
+
+use PHPUnit\Framework\TestCase;
+
+class setDefaultParametersTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
